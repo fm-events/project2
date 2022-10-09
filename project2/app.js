14e528e1f1ceeeb242e5212c9cf12c8246da05b3
mauro404 <mau.mansano@gmail.com>
2022-10-09 17:29:43 +0200

    create event routes and event views folder

--- project2/app.js
+++ project2/app.js
@@ -30,6 +30,8 @@ app.use("/", index);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+app.use("/", require("./routes/event.routes"));
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
---
+++ project2/routes/event.routes.js
@@ -0,0 +1,103 @@
+const router = require('express').Router();
+const Event = require("../models/Event.model");
+//const User = require("../models/User.model");
+
+const isLoggedIn = require("../middleware/isLoggedIn");
+
+//READ: List all events
+//get /events
+router.get("/events", (req, res, next) => {
+    Event.find()
+        .then((events) => {
+            res.render("events/events-list", {events: events})
+        })
+        .catch()
+});
+
+
+//CREATE: display form
+// get /events/create
+router.get("/events/create", isLoggedIn, (req, res, next) => {
+    res.render("events/create-event")
+});
+
+//CREATE: process form
+// post /events/create
+router.post("/events/create", isLoggedIn, (req, res, next) => {
+    const newEvent = {
+        title: req.body.title,
+        description: req.body.description,
+        location: req.body.location,
+        date: req.body.date,
+        organizer: req.body.organizer,
+        attendants: req.body.attendants,
+    }
+    Event.create(newEvent)
+        .then( () => res.redirect("/events"))
+        .catch( () => res.redirect("/events/create"))
+});
+
+//READ: Event Details
+//get events/:eventId
+router.get("/events/:eventId", isLoggedIn, (req, res, next) => {
+    const eventId = req.params.eventId;
+
+    Event.findbyId(eventId)
+        .then( (event) =>{
+            res.render("events/event-details", event)
+        })
+        .catch()
+});
+
+
+//UPDATE: display form
+// get /events/:eventId/edit
+router.get("/events/:eventId/edit", isLoggedIn, (req, res, next) => {
+    const eventId = req.params.eventId;
+    
+    Event.findById(eventId)
+        .then( (event) =>{
+            res.render("events/edit-event", event)
+        })
+        .catch()
+});
+
+//UPDATE: process form
+// post /events/:eventId/edit
+router.post("/events/:eventId/edit", isLoggedIn, (req, res, next) => {
+    const eventId = req.params.eventId;
+    
+    const newDetails = {
+        title: req.body.title,
+        description: req.body.description,
+        location: req.body.location,
+        date: req.body.date,
+        organizer: req.body.organizer,
+        attendants: req.body.attendants,
+    }
+    Event.findByIdAndUpdate(eventId, newDetails)
+        .then( () =>{
+            res.redirect(`/events/${eventId}`)
+        })
+        .catch()
+});
+
+//DELETE:
+// post /events/:eventId/delete
+
+router.post("/events/:eventId/delete", isLoggedIn, (req, res, next) => {
+
+    Book.findByIdAndDelete(req.params.bookId)
+      .then(() => {
+        res.redirect("/events");
+      })
+      .catch(err => {
+        console.log("Error deleting book...", err);
+        next();
+      });
+  
+  });
+  
+
+  module.exports = router;
+  
