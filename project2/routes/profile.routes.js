95bd414e3ba5d2f79a4e898f0c5481c527a532ba
Farzin khaledi <Khaledifarzin1@gmail.com>
2022-10-13 21:25:48 +0100

    add more styling to th

--- project2/routes/profile.routes.js
+++ project2/routes/profile.routes.js
@@ -1,64 +1,70 @@
 const router = require('express').Router();
-const User = require("../models/User.model");
+const User = require('../models/User.model');
 
-const isLoggedIn = require("../middleware/isLoggedIn");
+const isLoggedIn = require('../middleware/isLoggedIn');
 //const upload = require("../middleware/fileUploader");
 const fileUploader = require('../config/cloudinary.config');
 
 //READ: display user profile
 router.get('/profile', isLoggedIn, (req, res, next) => {
-    const userId = req.session.user._id;
+  const userId = req.session.user._id;
 
-    User.findById(userId)
-        .then( (user) => {
-            res.render('profile/profile-page', user);
-        })
-        .catch(err => console.log(err))
-        
+  User.findById(userId)
+    .then((user) => {
+      res.render('profile/profile-page', user);
+    })
+    .catch((err) => console.log(err));
 });
 
 //UPDATE: display form
 router.get('/profile/edit', isLoggedIn, (req, res, next) => {
-    const userId = req.session.user._id;
+  const userId = req.session.user._id;
 
-    User.findById(userId)
-        .then( (user) => {
-            res.render('profile/edit-profile', user);
-        })
-        .catch(err => console.log(err))
+  User.findById(userId)
+    .then((user) => {
+      res.render('profile/edit-profile', user);
+    })
+    .catch((err) => console.log(err));
 });
 
 //UPDATE: process form
-router.post('/profile/edit', isLoggedIn, fileUploader.single("profilePicture"), (req, res, next) => {
+router.post(
+  '/profile/edit',
+  isLoggedIn,
+  fileUploader.single('profilePicture'),
+  (req, res, next) => {
     const userId = req.session.user._id;
-    
-    const {username, email} = req.body;
+
+    const { username, email } = req.body;
 
     let profilePicture;
-        if (req.file) {
-            profilePicture = req.file.path;
-        } else {
-            profilePicture = existingImage;
-        }
+    if (req.file) {
+      profilePicture = req.file.path;
+    } else {
+      profilePicture = existingImage;
+    }
 
-      User.findByIdAndUpdate(userId, {username, email, profilePicture}, {new: true})
-        .then(() => {
-          res.redirect(`/profile`);
-        })
-        .catch(err => console.log(err));
-});
+    User.findByIdAndUpdate(
+      userId,
+      { username, email, profilePicture },
+      { new: true }
+    )
+      .then(() => {
+        res.redirect(`/profile`);
+      })
+      .catch((err) => console.log(err));
+  }
+);
 
 //DELETE: user profile
 router.post('/profile/delete', isLoggedIn, (req, res, next) => {
-    const userId = req.session.user._id;
+  const userId = req.session.user._id;
 
-    User.findByIdAndDelete(userId)
-        .then( () => {
-            res.redirect('/')
-        })
-        .catch(err => console.log(err))
+  User.findByIdAndDelete(userId)
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch((err) => console.log(err));
 });
 
-
 module.exports = router;
-  
